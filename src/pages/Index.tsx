6127398a84a30525b782b4588c631bdded16d52e
aradhyasharma894-tech <aradhyasharma894@gmail.com>
2026-02-02 16:11:15 +0530

    updated google review

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,12 +5,17 @@ import { WhyChooseUsSection } from "@/components/WhyChooseUsSection";
 import { QuoteFormSection } from "@/components/QuoteFormSection";
 import { TermsSection } from "@/components/TermsSection";
 import { ContactSection } from "@/components/ContactSection";
+import GoogleReviewsSection from "@/components/GoogleReviewsSection";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+
+      {/* Google Reviews – Trust Booster */}
+      <GoogleReviewsSection />
+
       <HowItWorksSection />
       <WhyChooseUsSection />
       <QuoteFormSection />
