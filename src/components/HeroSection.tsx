e72786cd5f4afd1e3b19344b4d57059ef59efa8d
aradhyasharma894-tech <aradhyasharma894@gmail.com>
2026-02-03 21:33:07 +0530

    color change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export const HeroSection = () => {
   return (
     <section
       id="home"
-      className="relative overflow-hidden bg-gradient-to-br from-primary-light to-secondary-light"
+      className="relative overflow-hidden bg-white"
     >
       {/* Decorative blur */}
       <div className="absolute -top-32 -right-32 w-96 h-96 bg-primary/20 rounded-full blur-3xl" />
